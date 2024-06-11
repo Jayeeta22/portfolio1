3cd227ee6fa38a6f9c6ee59643b5058c53e359eb
Jayeeta22 <jayeeta97.riya@gmail>
2024-06-11 21:42:49 +0530

    updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,32 +5,32 @@ const About = () => {
   return (
     <div name='about' className='about w-full bg-[] text-gray-300'>
       <div className='flex flex-col justify-center items-center w-full h-full'>
-        <div className='max-w-[1000px] w-full'>
-          <div className='text-start md:text-center  pb-6 pl-3'>
-            <p className='text-4xl font-bold inline' style={{color:"pink"}}>
-           𝒜𝒷𝑜𝓊𝓉 𝓂𝑒
+        <div className='max-w-[1000px] px-4  w-full'>
+          <div className='text-start md:text-center pb-6 pl-3 '>
+            <p className='text-4xl font-bold inline' style={{ color: "pink" }}>
+              𝒜𝒷𝑜𝓊𝓉 𝓂𝑒
             </p>
           </div>
-         
-          </div>
-          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-            <div className='sm:text-left text-4xl font-bold'>
-            <div class="wrapper six">
-        <div>
-            <p class="flicker">
-              Hi. I'm Jayeeta, nice to meet you guys!</p>
+
         </div>
-    </div>
-            
-            
+        <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+          <div className='sm:text-left text-4xl font-bold'>
+            <div class="wrapper six">
+              <div>
+                <p class="flicker">
+                  Hi. I'm Jayeeta, nice to meet you guys!</p>
+              </div>
             </div>
-            <div>
-              <p>I am passionate about building excellent software that improves
-              the lives of those around me. A motivated individual who is good at problem solving and programming, with an emphasis on writing 
-            clean and maintainable code. Experienced with data structure/algorithm and
-            designing optimal solution.</p>  
-            </div>
+
+
           </div>
+          <div>
+            <p>I am passionate about building excellent software that improves
+              the lives of those around me. A motivated individual who is good at problem solving and programming, with an emphasis on writing
+              clean and maintainable code. Experienced with data structure/algorithm and
+              designing optimal solution.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
