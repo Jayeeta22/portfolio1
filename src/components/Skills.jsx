2e42674e4880d4df6a35c01ac88ec3f12412e1b1
Jayeeta22 <jayeeta97.riya@gmail>
2024-06-19 12:43:17 +0530

    updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -61,7 +61,7 @@ const Skills = () => {
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={Nextjs} alt="HTML icon" />
-                  <p className='my-4'>FIREBASE</p>
+                  <p className='my-4'>NEXTJS</p>
               </div>
           </div>
       </div>
