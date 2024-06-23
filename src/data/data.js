51693fb5ebf21e02e1021bea2479ad1dc4357292
Jayeeta22 <jayeeta97.riya@gmail>
2024-06-23 20:34:03 +0530

    project updated

--- src/data/data.js
+++ src/data/data.js
@@ -3,6 +3,7 @@ import instaclone from '../assets/projects/instaclone.jpeg'
 import laundry from '../assets/projects/laundry.jpeg'
 import ceraphic from '../assets/projects/ceraphic.png'
 import greencare from '../assets/projects/greenhedge.png'
+import google from '../assets/projects/google.png'
 
 
 export const data=[
@@ -17,6 +18,15 @@ export const data=[
     },
     {
         id:2,
+        title:"Google Search Clone ",
+        image:google,
+        description: 'Created a Google Search Engine clone using Google Programmable Search Engine API to search for data and images. Implemented a fallback mechanism using the Heroku API to fetch data when no relevant search results are found. Integrated Extreme IP Lookup for live geo-location tracking of users. Ensure seamless and accurate search experiences with up-to-date information.',
+        technologies: 'Next.js, Tailwind CSS, Programmable Search Engine Api, Heroku Api, Extreme-ip-lookup ',
+        github:"https://github.com/Jayeeta22/google-search-next",
+        live:"https://google-search-next.vercel.app/",
+    },
+    {
+        id:3,
         title:"Lawncare Website",
         image:greencare,
         description: 'GreenHedge Lawn Care offers eco-friendly treatments, seasonal packages, and custom landscaping designs. Easily book consultations to achieve a pristine, healthy lawn year-round.',
@@ -34,7 +44,7 @@ export const data=[
     //     live:"https://silver-fox-d506d8.netlify.app/",
     // },
     {
-        id:3,
+        id:4,
         title:"Laundry project",
         image:laundry,
         description: 'Laundry is your go-to solution for all laundry needs, offering the ability to add different types of clothes, choose various washing methods, and track your order. Enjoy easy scheduling and stay updated with delivery dates to ensure your items are clean and ready when you need them.',
@@ -42,15 +52,15 @@ export const data=[
         github:"https://github.com/Jayeeta22/Laundry-project-frontend",
         live:"https://cute-kitten-720341.netlify.app/",
     },
-    {
-        id:4,
-        title:"Full responsive instaclon app",
-        image:instaclone,
-        description: 'InstaClone is a social media platform where users can upload and share images with others. Connect with friends, explore new content, and share your moments effortlessly.',
-        technologies: 'ReactJS, NodeJS, Cloudinary',
-        github:"https://github.com/Jayeeta22/instaclone-frontend",
-        live:"https://stupendous-peony-cd6952.netlify.app/",
-    },
+    // {
+    //     id:4,
+    //     title:"Full responsive instaclon app",
+    //     image:instaclone,
+    //     description: 'InstaClone is a social media platform where users can upload and share images with others. Connect with friends, explore new content, and share your moments effortlessly.',
+    //     technologies: 'ReactJS, NodeJS, Cloudinary',
+    //     github:"https://github.com/Jayeeta22/instaclone-frontend",
+    //     live:"https://stupendous-peony-cd6952.netlify.app/",
+    // },
     
     
 
